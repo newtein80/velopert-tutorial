cd181b55043e7e97912d61a2b4766910b0167e76
nile <newtein80@gmail.com>
2019-03-29 16:23:57 +0900

    velopert-tutorial 02 end

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App"
+      // 태그 사이에
+      >
+        {/* 주석은 이렇게 */}
         Park Jong Soo
       </div>
     );
